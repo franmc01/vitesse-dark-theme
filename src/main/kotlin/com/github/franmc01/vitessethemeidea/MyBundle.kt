b73893213b41e99d768dc04c24d893418ef9b55f
GitHub Action <action@github.com>
2025-08-02 19:39:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/franmc01/vitessethemeidea/MyBundle.kt
+++ src/main/kotlin/com/github/franmc01/vitessethemeidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.franmc01.vitessethemeidea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/franmc01/vitessethemeidea/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/franmc01/vitessethemeidea/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.franmc01.vitessethemeidea.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.franmc01.vitessethemeidea.MyBundle
+import com.github.franmc01.vitessethemeidea.services.MyProjectService
 import javax.swing.JButton
 
 
